936c7e14ecabe7018838610a8c6ca472af062f68
Nullblank <nullblank@protonmail.com>
2022-08-31 17:58:28 +0800

    spacing cleanup

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -10,8 +10,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://cdn.tailwindcss.com"></script>
 	<title>GCM-IS Login</title>
-
-
 </head>
 
 <body class="bg-cover grid grid-cols-1 gap-4 p-40" style="background-image: url('assets/images/login_bg.png')">
