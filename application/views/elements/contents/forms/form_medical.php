367eee26b65a3629eafc9eaaf82c0527dec41b66
Frolyn Raguindin <frlynrag@gmail.com>
2022-12-13 19:15:20 +0800

    added frontend

--- application/views/elements/contents/forms/form_medical.php
+++ application/views/elements/contents/forms/form_medical.php
@@ -45,12 +45,12 @@
                                 <p><?php echo form_error('m_conf'); ?></p>
                             </div>
                         </div>
-                        <div class="flex flex-wrap -m-3">
+                        <div id="m_confdesc_field" class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly specify the reason/s (sickness/illness) why you were confined.</p>
                             </div>
                             <div class="w-full md:w-2/3 p-3">
-                                <textarea  name="m_confdesc" id="m_confdesc" class="h-64 w-full px-4 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-green-500 border border-coolGray-200 rounded-lg shadow-input resize-none" type="text" placeholder="Write here..."></textarea>
+                                <textarea name="m_confdesc" id="m_confdesc" class="h-64 w-full px-4 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-green-500 border border-coolGray-200 rounded-lg shadow-input resize-none" type="text" placeholder="Write here..."></textarea>
                             </div>
                         </div>
                     </div>
@@ -66,7 +66,7 @@
                                 <div class="relative">
                                     <svg class="absolute right-4 top-1/2 transform -translate-y-1/2" width="16" height="16" viewbox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.3333 6.1133C11.2084 5.98913 11.0395 5.91943 10.8633 5.91943C10.6872 5.91943 10.5182 5.98913 10.3933 6.1133L8.00001 8.47329L5.64001 6.1133C5.5151 5.98913 5.34613 5.91943 5.17001 5.91943C4.99388 5.91943 4.82491 5.98913 4.70001 6.1133C4.63752 6.17527 4.58792 6.249 4.55408 6.33024C4.52023 6.41148 4.50281 6.49862 4.50281 6.58663C4.50281 6.67464 4.52023 6.76177 4.55408 6.84301C4.58792 6.92425 4.63752 6.99799 4.70001 7.05996L7.52667 9.88663C7.58865 9.94911 7.66238 9.99871 7.74362 10.0326C7.82486 10.0664 7.912 10.0838 8.00001 10.0838C8.08801 10.0838 8.17515 10.0664 8.25639 10.0326C8.33763 9.99871 8.41136 9.94911 8.47334 9.88663L11.3333 7.05996C11.3958 6.99799 11.4454 6.92425 11.4793 6.84301C11.5131 6.76177 11.5305 6.67464 11.5305 6.58663C11.5305 6.49862 11.5131 6.41148 11.4793 6.33024C11.4454 6.249 11.3958 6.17527 11.3333 6.1133Z" fill="#8896AB"></path>
-                                    </svg><select name="m_medill" class="appearance-none w-full py-2.5 px-4 text-coolGray-900 text-base font-normal bg-white border outline-none border-coolGray-200 focus:border-green-500 rounded-lg shadow-input">
+                                    </svg><select id="m_medill" name="m_medill" class="appearance-none w-full py-2.5 px-4 text-coolGray-900 text-base font-normal bg-white border outline-none border-coolGray-200 focus:border-green-500 rounded-lg shadow-input">
                                         <option selected value="0">No</option>
                                         <option value="1">Yes</option>
                                     </select>
@@ -74,7 +74,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="w-full md:w-9/12">
+                    <div id="m_medcon_field" class="w-full md:w-9/12">
                         <div class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly check all that apply</p>
@@ -228,7 +228,7 @@
                     </div>
                 </div>
 
-                <div class="py-6 border-b">
+                <div id="m_medconO_field" class="py-6 border-b">
                     <div class="w-full md:w-9/12">
                         <div class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
@@ -241,7 +241,7 @@
                     </div>
                 </div>
 
-                <div class="py-6 border-b border-coolGray-100">
+                <div id="m_medwhen_field" class="py-6 border-b border-coolGray-100">
                     <div class="w-full md:w-9/12">
                         <div class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
@@ -284,7 +284,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-wrap -m-3">
+                        <div id="m_presmeddesc_field" class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly specify the sickness/medical concern that your medication is for:</p>
                             </div>
@@ -307,7 +307,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-wrap -m-3">
+                        <div id="m_pastmeddesc_field" class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly specify the sickness/medical concern that your medication was for:</p>
                             </div>
@@ -335,7 +335,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-wrap -m-3">
+                        <div id="m_disdesc_field" class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly specify your Disability</p>
                             </div>
@@ -343,7 +343,7 @@
                                 <textarea name="m_disdesc" id="m_disdesc" class="h-64 w-full px-4 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-green-500 border border-coolGray-200 rounded-lg shadow-input resize-none" type="text" placeholder="Write here..."></textarea>
                             </div>
                         </div>
-                        <div class="flex flex-wrap -m-3">
+                        <div id="m_disass_field" class="flex flex-wrap -m-3">
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, has your Disability been diagnosed or assessed by a medical proffessional?</p>
                             </div>
@@ -362,7 +362,7 @@
                             <div class="w-full md:w-1/3 p-3">
                                 <p class="text-sm text-coolGray-800">If Yes, kindly specify the Month and Year:</p>
                             </div>
-                            <div class="w-full md:w-1/3 p-3">
+                            <div id="m_dismonth_field" class="w-full md:w-1/3 p-3">
                                 <div class="relative">
                                     <svg class="absolute right-4 top-1/2 transform -translate-y-1/2" width="16" height="16" viewbox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M11.3333 6.1133C11.2084 5.98913 11.0395 5.91943 10.8633 5.91943C10.6872 5.91943 10.5182 5.98913 10.3933 6.1133L8.00001 8.47329L5.64001 6.1133C5.5151 5.98913 5.34613 5.91943 5.17001 5.91943C4.99388 5.91943 4.82491 5.98913 4.70001 6.1133C4.63752 6.17527 4.58792 6.249 4.55408 6.33024C4.52023 6.41148 4.50281 6.49862 4.50281 6.58663C4.50281 6.67464 4.52023 6.76177 4.55408 6.84301C4.58792 6.92425 4.63752 6.99799 4.70001 7.05996L7.52667 9.88663C7.58865 9.94911 7.66238 9.99871 7.74362 10.0326C7.82486 10.0664 7.912 10.0838 8.00001 10.0838C8.08801 10.0838 8.17515 10.0664 8.25639 10.0326C8.33763 9.99871 8.41136 9.94911 8.47334 9.88663L11.3333 7.05996C11.3958 6.99799 11.4454 6.92425 11.4793 6.84301C11.5131 6.76177 11.5305 6.67464 11.5305 6.58663C11.5305 6.49862 11.5131 6.41148 11.4793 6.33024C11.4454 6.249 11.3958 6.17527 11.3333 6.1133Z" fill="#8896AB"></path>
@@ -384,7 +384,7 @@
                                     <p><?php echo form_error('m_dismonth'); ?></p>
                                 </div>
                             </div>
-                            <div class="w-full md:w-1/3 p-3">
+                            <div id="m_disyear_field" class="w-full md:w-1/3 p-3">
                                 <div class="relative">
                                     <input min="1995" minlength="4" type="number" placeholder="Year" id="m_disyear" name="m_disyear" class="appearance-none w-full py-2.5 px-4 text-coolGray-900 text-base font-normal bg-white border outline-none border-coolGray-200 focus:border-green-500 rounded-lg shadow-input"></input>
                                     <p><?php echo form_error('m_disyear'); ?></p>
@@ -398,3 +398,66 @@
         </div>
     </div>
 </form>
+
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        function init() {
+            const fields = [{
+                    id: "#m_conf",
+                    related_fields: [
+                        "#m_confdesc_field"
+                    ]
+                },
+                {
+                    id: "#m_medill",
+                    related_fields: [
+                        "#m_medcon_field", "#m_medconO_field", "#m_medwhen_field"
+                    ]
+                },
+                {
+                    id: "#m_presmed",
+                    related_fields: [
+                        "#m_presmeddesc_field"
+                    ]
+                },
+                {
+                    id: "#m_pastmed",
+                    related_fields: [
+                        "#m_pastmeddesc_field"
+                    ]
+                },
+                {
+                    id: '#m_dis',
+                    related_fields: [
+                        "#m_disdesc_field", "#m_disass_field", "#m_dismonth_field", "#m_disyear_field"
+                    ]
+                }
+            ]
+
+
+            fields.forEach((field) => {
+                const {
+                    id,
+                    related_fields
+                } = field
+
+                // Hide related fields initially.
+                related_fields.forEach((rf) => $(rf).hide())
+
+                // Show related fields if answer is "Yes".
+                $(id).change((e) => {
+                    const val = e.target.value
+                    if (val === '1')
+                        related_fields.forEach((rf) => $(rf).show())
+                    else
+                        related_fields.forEach((rf) => $(rf).hide())
+
+                })
+            })
+
+        }
+
+        init();
+    })
+</script>
